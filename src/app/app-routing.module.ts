6ba09ef30a62b6716de66dd291cf2edfbf933dc5
Moe Alali <moe3l3ali@gmail.com>
2020-08-10 20:05:26 +0300

    our services module and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'services', component: OurServicesComponent}
 ];
 
 
