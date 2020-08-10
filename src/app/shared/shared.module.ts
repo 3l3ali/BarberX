2f144ca5d8c7b089d90e728ee3146590c0063d39
Moe Alali <moe3l3ali@gmail.com>
2020-08-10 19:56:25 +0300

    price list component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { BannerComponent } from './components/banner/banner.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { RouterModule } from '@angular/router';
+import { PriceListComponent } from './components/price-list/price-list.component';
 
 
 
 @NgModule({
-  declarations: [BannerComponent, CarouselComponent],
+  declarations: [BannerComponent, CarouselComponent, PriceListComponent],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
     BannerComponent,
-    CarouselComponent
+    CarouselComponent,
+    PriceListComponent
   ]
 })
 export class SharedModule { }
