37eddf14b5d44219ab1514622a1afa866a532a83
Moe Alali <moe3l3ali@gmail.com>
2020-08-11 03:29:06 +0300

    services banner

--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-our-services',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OurServicesComponent implements OnInit {
 
-  constructor() { }
+  constructor(title: Title) {
+    title.setTitle('BarberX - Our Services');
+  }
 
   ngOnInit(): void {
   }
